9d00068a58eb05bebaf2f686d38252282658b03c
eothein <jens.buysse@hogent.be>
2017-09-26 13:56:45 +0200

    Create Base directory structure and base models

---
+++ app/src/main/java/com/example/eothein/a2048/models/Element.java
@@ -0,0 +1,31 @@
+package com.example.eothein.a2048.models;
+
+/**
+ * Created by Jens Buysse & Karine Samyn on 26.09.17.
+ */
+
+public class Element {
+
+
+    /**
+     * Number representing the element. An element representing an empty value receives the value of
+     * -1.
+     */
+    private int number = -1;
+
+    /**
+     * Returns the number of this element
+     * @return the number of this element
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * Sets the number for this element
+     * @param number the number of this element
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ app/src/main/java/com/example/eothein/a2048/models/Grid.java
@@ -0,0 +1,21 @@
+package com.example.eothein.a2048.models;
+
+/**
+ * Created by Jens Buysse & Karine Samyn on 26.09.17.
+ */
+
+public class Grid {
+
+
+    /**
+     * The size of the grid
+     */
+    private int gridSize;
+
+    private Element[][] grid;
+
+
+
+
+
+}
